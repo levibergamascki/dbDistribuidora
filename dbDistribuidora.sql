cc0a8bc1e72b572210e1c36058a4285c8dad59c4
Levi Bergamascki <82347503+levibergamascki@users.noreply.github.com>
2022-05-30 12:13:30 -0300

    criações de novas tabelas e alterações
    15 tabelas até o momento, com erro 1075 a ser solucionado na tabela tbcidade

--- dbDistribuidora.sql
+++ dbDistribuidora.sql
@@ -1,79 +1,129 @@
-create database dbDistribuidora;
-use dbDistribuidora;
 
--- entrada (compra)
-create table tbCompra(
-idCompra int primary key auto_increment,
-quantidadeCompra int not null,
-valorCompra float(6,2),
-idproduto int primary key auto_increment,
-foreign key(idproduto) references tbProduto(idproduto),
-idfornecedor int primary key auto_increment,
-foreign key (idfornecedor) references tbFornecedor(idfornecedor)
-);
-
--- saída (venda)
-create table tbVenda(
-idVenda int primary key auto_increment,
-quantidadeVenda int not null,
-valorVenda float(6,2),
-idproduto int primary key auto_increment,
-foreign key(idproduto) references tbProduto(idproduto),
-IdCliente int primary key auto_increment,
-foreign key (idcliente) references tbcliente(idcliente)
-);
-
--- cliente
-create table tbCliente(
-nomeCliente varchar(30) not null,
-Endereco varchar(50) not null,
-cep int not null,
-telefone int not null,
-email varchar(50) not null,
-IdCliente int primary key auto_increment
-);
-create table tbClientePfisica(
-cpf bigint not null,
-sexoCliente char(1) not null,
-IdCliente int primary key auto_increment,
-FOREIGN KEY (IdCliente) references tbCliente(IdCliente)
-);
-create table tbClientePjuridica(
-cnpj bigint not null,
-IdCliente int primary key auto_increment,
-FOREIGN KEY (IdCliente) references tbCliente(IdCliente)
-);
--- produto
-create table tbProduto(
-idProduto int primary key auto_increment,
-nomeProduto varchar(50) not null,
-valorunitario float(6,2) not null,
-estoqueProduto int not null
-);
-
--- fornecedor
-create table tbFornecedor(
-idfornecedor int primary key auto_increment
--- quantidade
--- tipo
--- valor 
--- 
-);
--- NotaFiscal
-create table tbNotaFiscal(
-registroNfiscal date,
-idVenda int primary key auto_increment,
-/*Razão Social
-CNPJ
-DDD
-Telefone
-Endereço
-Número
-Bairro
-Estado
-Cidade
-CEP
-E-Mail
-Inscrição Estadual*/
-foreign key (idVenda) references tbVenda(idvenda)
+create database dbdistribuidora; 
+ use dbdistribuidora; 
+  
+ create table tbestado( 
+         estadoUF char(2) not null, 
+         UFID int primary key auto_increment
+ ); 
+  
+ create table tbcidade( 
+	 idcidade int primary key auto_increment, 
+     cidade varchar(50), 
+     UFID int auto_increment, 
+     foreign key (UFId) references tbestado(UFId) 
+ ); 
+  
+ create table tblog( 
+     logradouro varchar(100) not null, 
+     bairro varchar(50), 
+     cep int primary key, 
+     idcidade int, 
+     foreign key (idcidade) references tbcidade(idcidade) 
+ ); 
+  
+ create table tbendereco( 
+         IDendereco int primary key auto_increment, 
+     compEndereco varchar(50), 
+     cep numeric(8), 
+     foreign key (cep) references tblog(cep) 
+ ); 
+  
+ create table tbpessoa( 
+         idCli int primary key auto_increment, 
+     nomeCli varchar(200) not null, 
+     telPessoa int not null, 
+         numLog numeric(6) not null, 
+     IDendereco int, 
+     foreign key (IDendereco) references tbendereco(IDendereco) 
+ ); 
+  
+ create table tbclientePJ( 
+         cnpj numeric(14) not null primary key, 
+     IE numeric(11) not null, 
+     idCli int, 
+     foreign key (idCli) references tbpessoa(idCli) 
+ ); 
+  
+ CREATE TABLE tbclientePF ( 
+     cpf numeric(11) NOT NULL PRIMARY KEY, 
+     dataNasc DATE not null, 
+     rg numeric(9) not null,
+     rgdig char(1) not null,
+	sexoCli char(1), 
+     idCli INT, 
+     FOREIGN KEY (idCli) 
+         REFERENCES tbpessoa (idCli) 
+ ); 
+  
+ ---------- 
+ create table tbfornecedor( 
+         codFornecedor numeric(10) auto_increment primary key, 
+     cnpj numeric(14) not null unique, 
+     nomeFornecedor varchar(200) not null, 
+     tel int
+ ); 
+  
+ create table tbproduto( 
+	 codigoBarras numeric(14) primary key unique, 
+     nomeProd varchar(200) not null, 
+     valorUnit decimal(7,2) not null, 
+     qtd int 
+ ); 
+  
+ create table tbprodutoComprado ( 
+		CodigoBarras bigint not null,
+		NotaFiscal int primary key,
+        valoritem decimal(7,2) not null,
+        qtd int not null,
+        foreign key (CodigoBarras) references tbproduto(codigobarras)
+		
+ ); 
+  
+ create table tbcompra( 
+         NotaFiscal int primary key not null, 
+     dataCompra date not null, 
+     valorTotal decimal(7,2) not null, 
+     qtdTotal int not null, 
+     codFornecedor int , 
+     codigoBarras bigint, 
+     foreign key (NotaFiscal) references tbprodutocomprado(NotaFiscal),
+     foreign key (codFornecedor) references tbfornecedor(codFornecedor), 
+     foreign key (codigoBarras) references tbproduto(codigoBarras) 
+ ); 
+  
+ create table tbestoque( 
+         idestoque int auto_increment primary key, 
+         qtd int, 
+     valorItem decimal(7,2), 
+     codigoBarras bigint, 
+     foreign key (codigoBarras) references tbproduto(codigoBarras) 
+ ); 
+  
+ ---------------------------- 
+  
+ create table tbNF( 
+         NF int primary key, 
+     totalNota decimal(7,2) not null, 
+     dataEmissao date not null, 
+     codigoBarras bigint, 
+     foreign key (codigoBarras) references tbproduto(codigoBarras) 
+ ); 
+  
+ create table tbvenda( 
+         numeroVenda int primary key, 
+     dataVenda datetime, 
+     totalVenda decimal(7,2) not null, 
+     NF int, 
+     idCli int not null, 
+     foreign key (NF) references tbNF(NF), 
+     foreign key (idCli) references tbpessoa(idCli) 
+ );
+create table tbItemVenda(
+numeroVenda int primary key,
+CodigoBarras numeric(14),
+ValorItem decimal(7,2) not null,
+qtd int not null,
+foreign key (numeroVenda) references tbvenda(numeroVenda),
+foreign key (CodigoBarras) references tbproduto(codigoBarras)
 )
